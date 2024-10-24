9320a86b26d52c17ea0fd28fdd88fc42214d5798
saiphanindra24 <saiphanindra.gampala@gmail.com>
2024-10-24 11:15:06 +0530

    added reset button

--- vite01/src/Counter.jsx
+++ vite01/src/Counter.jsx
@@ -7,22 +7,28 @@ export default function Counter(){
     let addValue = ()=>{
         if (count ==20){
             setCount(count=20)
+            alert("You have reached maximum limit")
         }else{
             setCount(count+1)
         }
     }
 
     let removeValue = ()=>{
-        if (count<=0){
-            setCount(count=0)
+        if (count<=-10){
+            setCount(count=-10)
+            alert("you have reaced minimum limit")
         }else{
             setCount(count-1)
         }
     }
+    let resetValue = ()=>{
+        setCount(count=0)
+    }
     return(
         <>
-        <h1>COUNTER:{count}</h1>
+        <h1 style = {{color: count>0?"green":count<0?"red":"white"}}>{count}</h1>
         <button onClick={addValue}>Increase</button>
+        <button onClick={resetValue} style={{color:"white"}}>Reset</button>
         <button onClick={removeValue}>Decrease</button>
         </>
     )
